dd4a2fbf2c8b5167f4f841baeff181c1ade77479
dkhongell <116841735+dkhongell@users.noreply.github.com>
2022-11-21 19:52:30 -0500

    Add files via upload

---
+++ Q52.py
@@ -0,0 +1,16 @@
+fin=open("text1.txt")
+
+fin1=fin.readline()
+print(fin1)
+fin2=fin.readline()
+print(fin2)
+
+fin3=fin.readline()
+print(fin3)
+
+fin4=fin.readline()
+print(fin4)
+
+def print_twice(fin):
+    print(fin1, fin2, fin3, fin4)
+    
